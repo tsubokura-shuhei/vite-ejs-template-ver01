451debdf118e209056b350ad790c05b679755582
tsubokura-shuhei <repeat2639@gmail.com>
2025-03-06 09:34:33 +0900

    file fix

--- vite.config.js
+++ vite.config.js
@@ -10,22 +10,19 @@ const jsFiles = Object.fromEntries(
   globSync("src/**/*.js", {
     ignore: ["node_modules/**", "**/modules/**", "**/dist/**"],
   }).map((file) => [
-    path.relative(
-      "src",
-      file.slice(0, file.length - path.extname(file).length)
-    ),
+    path.basename(file, path.extname(file)), // フォルダ構造を除外し、ファイル名のみ取得
     fileURLToPath(new URL(file, import.meta.url)),
   ])
 );
 
 const scssFiles = Object.fromEntries(
-  globSync("src/assets/css/pages/**/*.scss", {
-    ignore: ["src/assets/css/pages/**/_*.scss"],
+  globSync("src/assets/scss/**/*.scss", {
+    ignore: ["src/assets/scss/**/_*.scss"],
   }).map((file) => [
-    path.relative(
-      "src",
-      file.slice(0, file.length - path.extname(file).length)
-    ),
+    path
+      .relative("src/assets/scss/", file)
+      .replace(/\\/g, "/")
+      .replace(/\.[^/.]+$/, ""),
     fileURLToPath(new URL(file, import.meta.url)),
   ])
 );
@@ -33,14 +30,22 @@ const scssFiles = Object.fromEntries(
 const htmlFiles = Object.fromEntries(
   globSync("src/**/*.html", { ignore: ["node_modules/**", "**/dist/**"] }).map(
     (file) => [
-      path.relative(
-        "src",
-        file.slice(0, file.length - path.extname(file).length)
-      ),
+      path
+        .relative("src", file)
+        .replace(/\\/g, "/")
+        .replace(/\.[^/.]+$/, ""),
       fileURLToPath(new URL(file, import.meta.url)),
     ]
   )
 );
+
+// SCSS ファイルを取得（フォルダ構造を無視してファイル名のみ取得）
+// const scssFiles2 = Object.fromEntries(
+//   globSync("components/scss/*.scss").map((file) => [
+//     path.basename(file, path.extname(file)), // ファイル名のみ取得（フォルダ構造を排除）
+//     fileURLToPath(new URL(file, import.meta.url)), // 絶対パス
+//   ])
+// );
 
 const inputObject = { ...scssFiles, ...jsFiles, ...htmlFiles };
 
@@ -49,12 +54,21 @@ export default defineConfig({
   build: {
     outDir: "dist",
     cssCodeSplit: true,
+    modulePreload: false,
     rollupOptions: {
       input: inputObject,
       output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
+        entryFileNames: "assets/js/[name].js", // フォルダを作成しない
+        chunkFileNames: "assets/js/[name].js", // チャンクも同様
+        assetFileNames: (assetInfo) => {
+          if (/\.(gif|jpeg|jpg|png|svg|webp)$/.test(assetInfo.name)) {
+            return "assets/[name].[ext]";
+          }
+          if (/\.(css)$/.test(assetInfo.name)) {
+            return "assets/css/[name].[ext]";
+          }
+          return "assets/[name].[ext]";
+        },
       },
     },
   },
